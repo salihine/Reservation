9b4666ff4ccd04df94f7464bd3df250f51fe3940
fadal <mfadel@soteli.com>
2015-03-12 23:29:58 -0400

    Premier commit

---
+++ src/java/reservation/ws/HotelReservation.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reservation.ws;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+
+/**
+ * REST Web Service
+ *
+ * @author Ahmad
+ */
+@Path("hotel")
+public class HotelReservation {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of HotelReservation
+     */
+    public HotelReservation() {
+    }
+
+    /**
+     * Retrieves representation of an instance of reservation.ws.HotelReservation
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces("application/xml")
+    public String getXml() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of HotelReservation
+     * @param content representation for the resource
+     * @return an HTTP response with content of the updated or created resource.
+     */
+    @PUT
+    @Consumes("application/xml")
+    public void putXml(String content) {
+    }
+}
